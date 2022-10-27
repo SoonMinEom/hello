acfc61abea401701db0ed29b26d124590d9a3ded
soonmineom <skdlfma123@gmail.com>
2022-10-27 15:35:30 +0900

    Swagger 적용

--- src/main/java/com/springboot/hello/controller/PutController.java
+++ src/main/java/com/springboot/hello/controller/PutController.java
@@ -10,27 +10,26 @@ import java.util.Map;
 @RestController
 @RequestMapping("/api/v1/put-api")
 public class PutController {
+//
+//    @RequestMapping(value = "/domain",method = RequestMethod.POST)
+//    public String postExample() {
+//        return "Hello Post API";
+//    }
+//
+//    @PostMapping("/member")
+//    public String postMember(@RequestBody Map<String, Object> postData) {
+//        StringBuilder sb = new StringBuilder();
+//        postData.entrySet().forEach(map->sb.append(map.getKey()+":"+map.getValue()+"\n"));
+//        return sb.toString();
+//    }
+//
+//    @PostMapping("/member2")
+//    public String postMember2(@RequestBody MemberDto memberDto) {
+//      return memberDto.toString();
+//    }
 
-    @RequestMapping(value = "/domain",method = RequestMethod.POST)
-    public String postExample() {
-        return "Hello Post API";
-    }
-
-    @PostMapping("/member")
-    public String postMember(@RequestBody Map<String, Object> postData) {
-        StringBuilder sb = new StringBuilder();
-        postData.entrySet().forEach(map->sb.append(map.getKey()+":"+map.getValue()+"\n"));
-        return sb.toString();
-    }
-
-    @PostMapping("/member2")
-    public String postMember2(@RequestBody MemberDto memberDto) {
-      return memberDto.toString();
-    }
-
-    @PostMapping("/member3")
+    @PutMapping("/member3")
     public ResponseEntity<MemberDto> putMember(@RequestBody MemberDto memberDto) {
-
         return ResponseEntity
                 .status(HttpStatus.ACCEPTED)
                 .body(memberDto);
