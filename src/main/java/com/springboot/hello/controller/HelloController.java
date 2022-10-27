fabba94d8276279a11ee778a1844894aabcf8cc7
soonmineom <skdlfma123@gmail.com>
2022-10-27 11:49:31 +0900

    Map<>을 활용한 RequestParam 받아오기

--- src/main/java/com/springboot/hello/controller/HelloController.java
+++ src/main/java/com/springboot/hello/controller/HelloController.java
@@ -2,6 +2,8 @@ package com.springboot.hello.controller;
 
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Map;
+
 //dispatcherServlet이 Mapping해줄 Controller를 등록
 @RestController
 @RequestMapping("/api/v1/get-api")
@@ -32,5 +34,13 @@ public class HelloController {
     @GetMapping(value = "/request1")
     public String getNamVariable2(@RequestParam String name,@RequestParam String email,@RequestParam String organization ) {
         return String.format("%s %s %s", name, email, organization);
+    }
+
+    @GetMapping(value = "/request2")
+    public String getNamVariable3(@RequestParam Map<String, String> param) {
+        param.entrySet().forEach((map)->{
+            System.out.printf("Key : %s value : %s\n",map.getKey(),map.getValue());
+        });
+        return "request2 호출 완료!!";
     }
 }
