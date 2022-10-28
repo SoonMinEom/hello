ae5519a030debfba656f2ec76f7ceb120211dfe0
soonmineom <skdlfma123@gmail.com>
2022-10-28 15:47:51 +0900

    롬복 활용한 UserDto 클래스 작성

---
+++ src/main/java/com/springboot/hello/domain/dto/UserDto.java
@@ -0,0 +1,13 @@
+package com.springboot.hello.domain.dto;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class UserDto {
+    private String id;
+    private String name;
+    private String password;
+}
