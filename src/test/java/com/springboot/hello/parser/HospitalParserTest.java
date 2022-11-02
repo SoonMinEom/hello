f5deeeb2309a837be6095b3097b09656c1507574
soonmineom <skdlfma123@gmail.com>
2022-11-02 22:46:03 +0900

    @Service @Transactional stream().paralle().froeach 사용해서 10만개 데이터 빠르게 넣기

--- src/test/java/com/springboot/hello/parser/HospitalParserTest.java
+++ src/test/java/com/springboot/hello/parser/HospitalParserTest.java
@@ -2,6 +2,7 @@ package com.springboot.hello.parser;
 
 import com.springboot.hello.dao.HospitalDao;
 import com.springboot.hello.domain.Hospital;
+import com.springboot.hello.service.HospitalService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -22,15 +23,27 @@ import static org.junit.jupiter.api.Assertions.*;
 // 스프링부트로 테스트 할 경우에는 위에 두 개 안 쓰고.. 아래 하나만..(환경변수 설정 필수!!!)
 @SpringBootTest
 class HospitalParserTest {
-//
-//    @Autowired
-//    ReadLineContext<Hospital> hospitalReadLineContext;
-//
-//    @Autowired
-//    HospitalDao hospitalDao;
-//
-//    String line1 = "\"1\",\"의원\",\"01_01_02_P\",\"3620000\",\"PHMA119993620020041100004\",\"19990612\",\"\",\"01\",\"영업/정상\",\"13\",\"영업중\",\"\",\"\",\"\",\"\",\"062-515-2875\",\"\",\"500881\",\"광주광역시 북구 풍향동 565번지 4호 3층\",\"광주광역시 북구 동문대로 24, 3층 (풍향동)\",\"61205\",\"효치과의원\",\"20211115113642\",\"U\",\"2021-11-17 02:40:00.0\",\"치과의원\",\"192630.735112\",\"185314.617632\",\"치과의원\",\"1\",\"0\",\"0\",\"52.29\",\"401\",\"치과\",\"\",\"\",\"\",\"0\",\"0\",\"\",\"\",\"0\",\"\",";
-//    String Line770 = "";
+
+    @Autowired
+    ReadLineContext<Hospital> hospitalReadLineContext;
+
+    @Autowired
+    HospitalDao hospitalDao;
+
+    @Autowired
+    HospitalService hospitalService;
+
+    String filename = "C:\\Users\\안락한곳\\Downloads\\fulldata_01_01_02_P_의원1.csv";
+
+    @Test
+    void largeAddAndGet() {
+        hospitalDao.deletAll();
+        hospitalService.insertLargeVolumeHospitalData(filename);
+        assertTrue(hospitalDao.getCount() > 100000);
+    }
+
+    String line1 = "\"1\",\"의원\",\"01_01_02_P\",\"3620000\",\"PHMA119993620020041100004\",\"19990612\",\"\",\"01\",\"영업/정상\",\"13\",\"영업중\",\"\",\"\",\"\",\"\",\"062-515-2875\",\"\",\"500881\",\"광주광역시 북구 풍향동 565번지 4호 3층\",\"광주광역시 북구 동문대로 24, 3층 (풍향동)\",\"61205\",\"효치과의원\",\"20211115113642\",\"U\",\"2021-11-17 02:40:00.0\",\"치과의원\",\"192630.735112\",\"185314.617632\",\"치과의원\",\"1\",\"0\",\"0\",\"52.29\",\"401\",\"치과\",\"\",\"\",\"\",\"0\",\"0\",\"\",\"\",\"0\",\"\",";
+    String Line770 = "";
 
 //    @Test
 //    @DisplayName("Hospital이 잘 insert되는지, select도 잘 되는지")
@@ -65,7 +78,7 @@ class HospitalParserTest {
 //
 //        assertEquals(1, hospitalDao.getCount());
 //    }
-
+//
 //    @Test
 //    @DisplayName("10만건 이상의 데이터가 파싱 되었는지")
 //    void oneHundredThousandRows() throws IOException {
